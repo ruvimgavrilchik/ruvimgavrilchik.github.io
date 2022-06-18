6aca1cdc9742893c303794f8f97a3db3245a7cb0
Ruvim Gavrilchik <ruvim.gavrilchik0@gmail.com>
2022-06-18 16:14:39 -0700

    changed file type
    testing

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$hour = 12;
+//comment in php
+/*multi line php comment lool like java */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ruvim G</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+	
+   <!---      <script src="index.js"></script>     --->  
+
+    <h1>Ruviloops</h1>
+
+    <?php if ($hour < 12): ?>
+        Good morning
+    <?php elseif ($hour < 18): ?>
+        Good afternoon
+    <?php elseif ($hour < 22): ?>
+        Good evening
+    <?php else: ?>
+        Good night
+    <?php endif; ?>
+
+  </body>
+</html>
+
+
+<!--
+COMMENT IN HTML
+
+// endwhile 
+endfor
+endforeach
+endswitch
+endif -->
